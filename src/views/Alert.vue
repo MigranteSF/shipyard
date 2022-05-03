07719ebf921710c288a8fbe77613aa87a9023b9e
JavierGomez98 <javier.gomez.contact@gmail.com>
2022-05-03 12:48:59 -0500

    add: info alert

--- src/views/Alert.vue
+++ src/views/Alert.vue
@@ -23,6 +23,26 @@
 
         </div>
       </div>
+      <div class="mb-3">
+        <h2 class="mb-2">Info</h2>
+        <div class="mb-2 border p-3 rounded">
+
+          <div class="message-banner info">
+            <div class="limit-width">
+              <i class="fa-solid fa-circle-info mr-3"></i>
+              <p class="message-banner-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur officiis odit culpa impedit!</small>
+              </p>
+              <div class="flex message-banner-actions">
+                <button class="button button-secondary button-sm">Shipyard</button>
+                <button class="button button-primary button-sm ml-2">Shipyard</button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   </section>
 </template>
