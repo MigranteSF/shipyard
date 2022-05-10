8d8707269edc2ab0eafc90d4ee8646b41f37156d
JavierGomez98 <javier.gomez.contact@gmail.com>
2022-05-10 11:49:44 -0500

    Remove: Bootstrap-vue

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-import BootstrapVue from 'bootstrap-vue'
 import * as components from "./components";
 import { directives } from '../directives';
 import { registerComponents, registerDirectives } from "../utils/plugins";
@@ -6,7 +5,6 @@ import '../assets/scss/main.scss';
 
 const Shipyard = {
     install(Vue) {
-        Vue.use(BootstrapVue);
         registerComponents(Vue, components)
         registerDirectives(Vue, directives)
     }
