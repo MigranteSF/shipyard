b1925c1234bf1ba2fb5ebb5e247883485bc3dabc
JavierGomez98 <javier.gomez.contact@gmail.com>
2022-07-14 14:17:14 -0500

    feat: alert success

--- src/views/Alert.vue
+++ src/views/Alert.vue
@@ -75,6 +75,44 @@
 
         </div>
       </div>
+      <div class="mb-3">
+        <h2 class="mb-2">Success</h2>
+        <div class="mb-2 border p-3 rounded">
+
+          <h5><b>Banner</b></h5>
+          <div class="message-banner success mb-4">
+            <div class="limit-width">
+              <i class="fa-solid fa-circle-info mr-3"></i>
+              <p class="message-banner-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur officiis odit culpa impedit!</small>
+              </p>
+              <div class="flex message-banner-actions">
+                <button class="button button-secondary button-sm">Shipyard</button>
+                <button class="button button-primary button-sm ml-2">Shipyard</button>
+              </div>
+            </div>
+          </div>
+
+          <h5><b>Card</b></h5>
+          <div class="message-card success">
+            <div class="limit-width">
+              <em class="fas fa-check-circle mr-3 success-100"></em>
+              <p class="message-banner-text">
+                <span class="neutral-70 regular">
+                  ¡Ya esta pre-aprobado!. Solo verifica el <b>stock</b> y
+                   solicita los <b>documentos</b> faltantes para que la <b>simulacion</b> #123456 pase a <b>Admision</b>.
+                </span>
+              </p>
+              <div class="flex message-banner-actions">
+                <button class="button button-secondary">Shipyard</button>
+                <button class="button button-primary ml-2">Shipyard</button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   </section>
 </template>
